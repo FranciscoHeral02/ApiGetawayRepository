72640cc250d5d557ad2cab66eb873b0446c3c001
Francisco Hernandez <francisco.heral02@gmail.com>
2022-05-31 23:26:21 -0500

    francisco repo

--- src/test/java/com/nttdata/bootcamp/bank/getaway/MsApiGetawayApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/bank/getaway/MsApiGetawayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nttdata.bootcamp.bank.products;
+package com.nttdata.bootcamp.bank.getaway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
